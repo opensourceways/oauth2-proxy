5aa0d4365aebe3ebcf0c6c79b52c16ea029811a2
xwzQmxx <1499273991@qq.com>
2022-05-10 10:04:30 +0800

    fix review

--- oauthproxy.go
+++ oauthproxy.go
@@ -744,7 +744,6 @@ func (p *OAuthProxy) OAuthStart(rw http.ResponseWriter, req *http.Request) {
 func (p *OAuthProxy) OAuthCallback(rw http.ResponseWriter, req *http.Request) {
 	remoteAddr := ip.GetClientString(p.realClientIPParser, req, true)
 
-	logger.Error(req, remoteAddr)
 	// finish the oauth cycle
 	err := req.ParseForm()
 	if err != nil {
